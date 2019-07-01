8b7a9130cd64985e7d148fe221780b05ae828166
Timmy Kokke <mental_c@hotmail.com>
2019-07-01 23:03:15 +0200

    added bindings to the slider

--- Code/src/components/atoms/Slider.stories.js
+++ Code/src/components/atoms/Slider.stories.js
@@ -2,9 +2,11 @@ import { storiesOf } from '@storybook/vue';
 
 import ujSlider from './Slider';
 
+const min=-10, max=10, step=2, value=0;
+
 storiesOf('2.Atoms|uj-slider', module)
     .add('uj-slider', () => ({
         components: { ujSlider },
-        render(h) { return <uj-slider></uj-slider> }
+        render(h) { return <uj-slider min={min} max={max} step={step} value={value}></uj-slider> }
     })
 )
--- Code/src/components/atoms/Slider.vue
+++ Code/src/components/atoms/Slider.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="slider">
-    <input class="slider-input" type="range" />
+    <input class="slider-input" :value="value" :min="min" :max="max" :step="step" type="range" />
   </div>
 </template>
 
 <script>
 export default {
-  name: "uj-slider"
+  name: "uj-slider",
+  props: {
+    max: { type: Number, default: 127 },
+    min: { type: Number, default: 0 },
+    value: { type: Number, default: 64 },
+    step: { type: Number, default: 1 }
+  }
 };
 </script>
 
@@ -38,12 +44,12 @@ export default {
     box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
     background: $accent;
     border-radius: 1.3px;
-    border: 1px solid rgba(0,0,0,0.5);
+    border: 1px solid rgba(0, 0, 0, 0.5);
   }
 
   input[type="range"]::-webkit-slider-thumb {
     box-shadow: 0px 0px 5px #000000;
-    border: 1px solid rgba(0,0,0,0.5);
+    border: 1px solid rgba(0, 0, 0, 0.5);
     height: 32px;
     width: 32px;
     border-radius: 16px;
@@ -54,9 +60,8 @@ export default {
   }
 
   input[type="range"]:focus::-webkit-slider-thumb {
-   border: 1px solid $accent-lightest;
+    border: 1px solid $accent-lightest;
     background: $accent-lighter;
-   
   }
 }
 </style>
