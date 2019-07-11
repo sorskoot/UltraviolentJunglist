ba9600357730561a2c178684e3252ba381a345dc
Timmy Kokke <mental_c@hotmail.com>
2019-07-11 22:59:47 +0200

    added @ alias to storybook

--- Code/.storybook/webpack.config.js
+++ Code/.storybook/webpack.config.js
@@ -1,20 +1,23 @@
 const path = require('path');
 
-module.exports = ({config}) => {
+module.exports = ({ config }) => {
+   
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src');
+
     config.module.rules.push({
-    test: [/\.stories\.js$/, /index\.js$/],
-    loaders: [require.resolve('@storybook/addon-storysource/loader')],
-    include: [path.resolve(__dirname, '../src/components')],
-    enforce: 'pre',
-  });
+        test: [/\.stories\.js$/, /index\.js$/],
+        loaders: [require.resolve('@storybook/addon-storysource/loader')],
+        include: [path.resolve(__dirname, '../src/components')],
+        enforce: 'pre',
 
-  config.module.rules.push(
-    {  
-        test: /\.scss$/,  
-        loaders: ["style-loader", "css-loader", "sass-loader"],  
-        include: path.resolve(__dirname, "../")  
-      } 
-  );
+    });
 
-  return config;
+    config.module.rules.push(
+        {
+            test: /\.scss$/,
+            loaders: ["style-loader", "css-loader", "sass-loader"],
+            include: path.resolve(__dirname, "../"),
+        }
+    );
+    return config;
 };
--- Code/src/components/atoms/Button.stories.js
+++ Code/src/components/atoms/Button.stories.js
@@ -1,49 +1,48 @@
 import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
-import { withNotes } from '@storybook/addon-notes';
+import { withInfo } from 'storybook-addon-vue-info';
 
 import ujButton from './Button';
 
 storiesOf('2.Atoms|uj-button', module)
-  .addDecorator(withNotes)
-  .add('outline', () => ({
-    components: { ujButton },
-    render(h) { return<uj-button >Outline Button</uj-button> },
-    methods: { action: action('clicked') },
-  }),
-  {notes: 'example notes on a button' })
-  .add('outline disabled', () => ({
-    components: { ujButton },
-    render(h) { return<uj-button disabled>Outline Button</uj-button> }
-  }))
+    .addDecorator(withInfo)
+    .add('outline', () => ({
+        components: { ujButton },
+        render(h) { return <uj-button >Outline Button</uj-button> },
+        methods: { action: action('clicked') },
+    }), { info: { summary: 'Summary for uj-button' } })
+    .add('outline disabled', () => ({
+        components: { ujButton },
+        render(h) { return <uj-button disabled>Outline Button</uj-button> }
+    }), { info: { summary: 'Summary for uj-button' } })
 
-  .add('primary', () => ({
-    components: { ujButton },    
-    render(h) { return <uj-button class="button-primary">Primary Button</uj-button> }
-  }))
-  .add('primary disabled', () => ({
-    components: { ujButton },    
-    render(h) { return <uj-button disabled class="button-primary">Primary Button</uj-button> }
-  }))
+    .add('primary', () => ({
+        components: { ujButton },
+        render(h) { return <uj-button class="button-primary">Primary Button</uj-button> }
+    }), { info: { summary: 'Summary for uj-button' } })
+    .add('primary disabled', () => ({
+        components: { ujButton },
+        render(h) { return <uj-button disabled class="button-primary">Primary Button</uj-button> }
+    }), { info: { summary: 'Summary for uj-button' } })
 
-  .add('success', () => ({
-    components: { ujButton },    
-    render(h) { return <uj-button class="button-success">Success Button</uj-button> }
-  }))
-  .add('success disabled', () => ({
-    components: { ujButton },    
-    render(h) { return <uj-button disabled class="button-success">Success Button</uj-button> }
-  }))
+    .add('success', () => ({
+        components: { ujButton },
+        render(h) { return <uj-button class="button-success">Success Button</uj-button> }
+    }), { info: { summary: 'Summary for uj-button' } })
+    .add('success disabled', () => ({
+        components: { ujButton },
+        render(h) { return <uj-button disabled class="button-success">Success Button</uj-button> }
+    }), { info: { summary: 'Summary for uj-button' } })
 
 
-  .add('danger', () => ({
-    components: { ujButton },    
-    render(h) { return <uj-button class="button-danger">Danger Button</uj-button> }
-  }))
-  .add('danger disabled', () => ({
-    components: { ujButton },    
-    render(h) { return <uj-button disabled class="button-danger">Danger Button</uj-button> }
-  }))
+    .add('danger', () => ({
+        components: { ujButton },
+        render(h) { return <uj-button class="button-danger">Danger Button</uj-button> }
+    }), { info: { summary: 'Summary for uj-button' } })
+    .add('danger disabled', () => ({
+        components: { ujButton },
+        render(h) { return <uj-button disabled class="button-danger">Danger Button</uj-button> }
+    }), { info: { summary: 'Summary for uj-button' } })
 
-  ;
+    ;
 
