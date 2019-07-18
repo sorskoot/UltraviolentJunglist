4a185a522984829f1f3d15a8dafdf33a17d6969e
Timmy Kokke <mental_c@hotmail.com>
2019-07-18 22:35:10 +0200

    Fixed retrigger

--- Code/src/lib/models/segment.js
+++ Code/src/lib/models/segment.js
@@ -1,8 +1,8 @@
 import Tone from 'tone';
-import { settings } from '../';
+import { settings, transport } from '../';
 
 export class Segment {
-    
+
     key = -1;
     bufferStart = 0;
     bufferLength = 1;
@@ -35,49 +35,29 @@ export class Segment {
         this.bufferStart = start;
         this.bufferLength = length;
         this.bufferDuration = this.buffer.duration;
-        //this.buffer.slice(this.bufferStart * buffer.duration, this.bufferLength * buffer.duration);
-
-        // let buffer = this.player._buffer.get();
-        // let length = transport.beat();
-        // const duration = length / segmentToPlay.retrigger;
-
-        // for (let i = 0; i < segmentToPlay.retrigger; i++) {
-        //     let source = Tone.context.createBufferSource();
-        //     let freq = 2000;
-        //     let type = "highpass";
-        //     let Q = 0.5;
-        //     let filter = new Tone.Filter(freq, type);
-        //     filter.Q.value = Q;
-        //     filter.connect(Tone.context.destination);
-
-        //     source.connect(filter);
-        //     source.buffer = buffer;
-
-        //     let d = i * duration;
-        //     let n = Tone.context.currentTime + d;
-        //     source.start(
-        //         n,
-        //         this.currentSegments[0].duration * segmentToPlay.bufferStart,
-        //         this.currentSegments[0].duration *
-        //         (segmentToPlay.bufferLength / segmentToPlay.retrigger)
-        //     );
-        // }
-
 
     }
 
     trigger() {
-        let n = Tone.context.currentTime + settings.latency;
+        let length = transport.beat();
+        const duration = length / this.retrigger;
+        for (let i = 0; i < this.retrigger; i++) {
+            let bufferSource = Tone.context.createBufferSource();
+            bufferSource.connect(Tone.context.destination);
+            bufferSource.buffer = this.buffer;
 
-        let bufferSource = Tone.context.createBufferSource();
-        bufferSource.connect(Tone.context.destination);
-        bufferSource.buffer = this.buffer;
+            let d = i * duration;
+            let n = Tone.context.currentTime + settings.latency + d;
 
-        bufferSource.start(
-            n,
-            this.bufferStart * this.bufferDuration,
-            this.bufferLength * this.bufferDuration
-        )
+            bufferSource.start(
+                n,
+                this.bufferStart * this.bufferDuration,
+
+                (this.bufferLength / this.retrigger) * this.bufferDuration
+            )
+        }
+
+
 
         //this.buffer.stop (n + this.duration * this.bufferLength);
         // trigger the sourceBuffer
